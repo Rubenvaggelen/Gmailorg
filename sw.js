74fcef989f7ffb2ed274b74ac592a969682d65f5
Rubenvaggelen <ruben.vanaggelen@gmail.com>
2026-09-01 12:46:58 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "postbus-shell-v1";
+const CACHE = "postbus-shell-v2";
 const SHELL = ["./index.html", "./style.css", "./app.js", "./manifest.json", "./logo-mark.png"];
 
 self.addEventListener("install", (event) => {
