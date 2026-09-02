79e597c39dca152cd921266a4384f995133af550
Rubenvaggelen <ruben.vanaggelen@gmail.com>
2026-09-02 15:12:04 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,21 +1,25 @@
-const CACHE = "gmailorg-shell-v19";
+const CACHE = "gmailorg-shell-v20";
 const SHELL = ["./index.html", "./style.css", "./app.js", "./manifest.json", "./logo-mark.png"];
-
 self.addEventListener("install", (event) => {
   event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(SHELL)));
   self.skipWaiting();
 });
-
 self.addEventListener("activate", (event) => {
   event.waitUntil(
     caches.keys().then((keys) => Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k))))
   );
   self.clients.claim();
 });
-
 self.addEventListener("fetch", (event) => {
   if (event.request.url.includes("googleapis.com")) return;
   event.respondWith(
     caches.match(event.request).then((cached) => cached || fetch(event.request))
   );
+});
+self.addEventListener("message", (event) => {
+  if (event.data && event.data.type === "CLEAR_CACHE") {
+    event.waitUntil(
+      caches.keys().then((keys) => Promise.all(keys.map((k) => caches.delete(k))))
+    );
+  }
 });
