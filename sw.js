dcacf5ccb08b058d102cc00446c92237991aa406
Rubenvaggelen <ruben.vanaggelen@gmail.com>
2026-09-03 20:47:27 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "gmailorg-shell-v40";
+const CACHE = "gmailorg-shell-v41";
 const SHELL = ["./index.html", "./style.css", "./app.js", "./manifest.json", "./logo-mark.png"];
 
 self.addEventListener("install", (event) => {
